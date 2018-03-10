38777a7bcebf4a4184a0f24456b4ce0ace0ce6fb
Clément B <clement.bouder@outlook.com>
2018-03-10 15:10:09 +0100

    Modification de la Vue Connexion

--- app/src/main/java/com/projet/iagenda/iagenda/VueConnexion.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueConnexion.java
@@ -1,11 +1,13 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class VueConnexion extends AppCompatActivity {
 
@@ -16,14 +18,25 @@ public class VueConnexion extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton boutonAnnuler = (FloatingActionButton) findViewById(R.id.boutonAnnuler);
+        boutonAnnuler.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(VueConnexion.this, MainActivity.class);
+                startActivity(intent);
             }
-        });*/
+        });
+
+
+        FloatingActionButton boutonConnexion = (FloatingActionButton) findViewById(R.id.boutonConnexion);
+        boutonConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueConnexion.this, VueCalendrierCache.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
