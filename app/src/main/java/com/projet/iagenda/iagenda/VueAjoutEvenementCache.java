47aadae6c256a7d877bab75bb4c67da3ad3551a4
Clément B <clement.bouder@outlook.com>
2018-03-10 15:20:16 +0100

    Création de la vue Ajout Evenement Cache et modification de la vue Calendrier Cache

---
+++ app/src/main/java/com/projet/iagenda/iagenda/VueAjoutEvenementCache.java
@@ -0,0 +1,13 @@
+package com.projet.iagenda.iagenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueAjoutEvenementCache extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue_ajout_evenement_cache);
+    }
+}
--- app/src/main/java/com/projet/iagenda/iagenda/VueCalendrierCache.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueCalendrierCache.java
@@ -1,7 +1,10 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VueCalendrierCache extends AppCompatActivity {
 
@@ -9,5 +12,33 @@ public class VueCalendrierCache extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vue_calendrier_cache);
+
+        FloatingActionButton boutonSortirModeCache = (FloatingActionButton) findViewById(R.id.boutonSortirModeCache);
+        boutonSortirModeCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueCalendrierCache.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton bouttonAjouterEvenementCache = (FloatingActionButton) findViewById(R.id.bouttonAjouterEvenementCache);
+        bouttonAjouterEvenementCache.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueCalendrierCache.this, VueAjoutEvenementCache.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton bouttonAfficherVueListeCachee = (FloatingActionButton) findViewById(R.id.bouttonAfficherVueListeCachee);
+        bouttonAfficherVueListeCachee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueCalendrierCache.this, VueListeCachee.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
