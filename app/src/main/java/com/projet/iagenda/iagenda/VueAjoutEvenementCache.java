d7a89ab20dd5c2edb31bee714add62f75c25f8b4
Clément B <clement.bouder@outlook.com>
2018-03-10 15:56:09 +0100

    Modification des évnements de la Vue Calendrier Cache

--- app/src/main/java/com/projet/iagenda/iagenda/VueAjoutEvenementCache.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueAjoutEvenementCache.java
@@ -1,7 +1,10 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VueAjoutEvenementCache extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class VueAjoutEvenementCache extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vue_ajout_evenement_cache);
+
+        FloatingActionButton boutonValider = (FloatingActionButton) findViewById(R.id.boutonValider);
+        boutonValider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueAjoutEvenementCache.this, VueConsulterEvenementCache.class);
+                startActivity(intent);
+            }
+        });
+
+        FloatingActionButton boutonRetour = (FloatingActionButton) findViewById(R.id.boutonRetour);
+        boutonRetour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueAjoutEvenementCache.this, VueCalendrierCache.class);
+                startActivity(intent);
+            }
+        });
     }
 }
