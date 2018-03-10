c92e5afd4ec29cbfdcf1ae1ce29711628b34570f
Clément B <clement.bouder@outlook.com>
2018-03-10 13:50:16 +0100

    Ajout de la vue Modifier Evenement

--- app/src/main/java/com/projet/iagenda/iagenda/VueConsulterEvenement.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueConsulterEvenement.java
@@ -1,13 +1,26 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VueConsulterEvenement extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vue_consulter_evenement);
+        FloatingActionButton boutonModifierEvenement = (FloatingActionButton) findViewById(R.id.boutonModifierEvenement);
+        boutonModifierEvenement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VueConsulterEvenement.this, VueAjoutEvenement.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/projet/iagenda/iagenda/VueModifierEvenement.java
@@ -0,0 +1,13 @@
+package com.projet.iagenda.iagenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueModifierEvenement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue_modifier_evenement);
+    }
+}
