892fa825863b5fb042a8519f759804ebdb96db2f
sofianeabi <sofiane.abidi1@gmail.com>
2018-03-11 17:12:27 +0100

    Ajout boutton pour travail Clément

---
+++ app/src/main/java/com/projet/iagenda/iagenda/VueTableauDeChasse.java
@@ -0,0 +1,17 @@
+package com.projet.iagenda.iagenda;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sofiane on 11/03/2018.
+ */
+
+public class VueTableauDeChasse extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vue_suppression_evenement);
+
+    }
+}
