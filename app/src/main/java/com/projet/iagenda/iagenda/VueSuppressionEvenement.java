2fd8cabd4ede88a39c195cea7d35cfc7167b2387
sofianeabi <sofiane.abidi1@gmail.com>
2018-04-08 14:56:06 +0200

    Ajout de message lors de la suppression d'evenement, d'evenement caché. Modification du message de suppression de contact (prise en compte du nom et prenom)

--- app/src/main/java/com/projet/iagenda/iagenda/VueSuppressionEvenement.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueSuppressionEvenement.java
@@ -1,7 +1,9 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,10 +38,37 @@ public class VueSuppressionEvenement extends AppCompatActivity {
 
         EditText texteHeureDebut = (EditText) findViewById(R.id.dateDebutVueSupprimerEvenement);
         texteHeureDebut.setText(MainActivity.heureRendezVous);
-        TextView textViewNom = (TextView) findViewById(R.id.champSelectionContactVueSupprimerEvenement);
+        final TextView textViewNom = (TextView) findViewById(R.id.champSelectionContactVueSupprimerEvenement);
         textViewNom.setText(MainActivity.personneRendezVous);
-        TextView textViewTitreEvenement = (TextView) findViewById(R.id.champTitreVueSupprimerEvenement);
+        final TextView textViewTitreEvenement = (TextView) findViewById(R.id.champTitreVueSupprimerEvenement);
         textViewTitreEvenement.setText(MainActivity.rendezVous);
+
+
+        findViewById(R.id.boutonSupprimerVueSupprimerEvenement).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(VueSuppressionEvenement.this);
+                builder.setCancelable(true);
+                builder.setTitle("Supprimer évenemment");
+                builder.setMessage("Voulez-vous réélement supprimer le " + textViewTitreEvenement.getText() + " avec " + textViewNom.getText() + " ?");
+
+
+                builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // alertTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+                builder.show();
+            }
+        });
 
     }
 
--- app/src/main/java/com/projet/iagenda/iagenda/VueSuppressionEvenementCache.java
+++ app/src/main/java/com/projet/iagenda/iagenda/VueSuppressionEvenementCache.java
@@ -1,7 +1,9 @@
 package com.projet.iagenda.iagenda;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -40,6 +42,33 @@ public class VueSuppressionEvenementCache extends AppCompatActivity {
         textViewNomCache.setText(VueCalendrierCache.personneRendezVousCache);
         TextView textViewTitreEvenementCache = (TextView) findViewById(R.id.champTitreVueSupprimerEvenementCache);
         textViewTitreEvenementCache.setText(VueCalendrierCache.rendezVousCache);
+
+
+        findViewById(R.id.boutonSupprimerVueSupprimerEvenementCache).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(VueSuppressionEvenementCache.this);
+                builder.setCancelable(true);
+                builder.setTitle("Supprimer évenemment caché");
+                builder.setMessage("Voulez-vous réélement supprimer le " + VueCalendrierCache.rendezVousCache + " avec " + VueCalendrierCache.personneRendezVousCache + " ?");
+
+
+                builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // alertTextView.setVisibility(View.VISIBLE);
+                    }
+                });
+                builder.show();
+            }
+        });
 
     }
 }
